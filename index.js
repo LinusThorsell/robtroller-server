4127538f8b603398af5dee63b12aea0d5fc3edfb
BuildTools <unconfigured@null.spigotmc.org>
2020-02-05 21:07:18 +0100

    added example server

---
+++ index.js
@@ -0,0 +1,15 @@
+function sendDataToServer(fullIPAndPort, dataToSend) {
+    let socket = new WebSocket(fullIPAndPort);
+    console.log("[Communications] : Opening communications")
+    
+    socket.onopen = function (e) {
+        socket.send(dataToSend);
+        console.log("[Communications] : Sent data successfully")
+        socket.close();
+    };
+    socket.onclose = function (event) {
+        console.log("[Communications] : Closed communications")
+    };
+}
+
+sendDataToServer("ws://192.168.1.167:5555/", "rotatehead(forward);turn(left);move(forward);turn(left);");
